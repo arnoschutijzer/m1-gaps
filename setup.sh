cba2ec432d49d15877fdf031f7f9ce7bc135034b
Arno Schutijzer <arno.schutijzer@skryv.com>
2021-06-01 10:58:44 +0200

    retag keycloak 13.0.0

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 cd keycloak-containers/server
 git checkout 13.0.0
-docker build . -t arnoschutijzer/keycloak:13.0.0
+docker build . -t keycloak:13.0.0
 git checkout 9.0.3
 docker build . -t keycloak:9.0.3
 
