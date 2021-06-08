26f592f7e5899f597805ffbc3503df96f384b842
Arno Schutijzer <arno.schutijzer@skryv.com>
2021-06-08 10:07:55 +0200

    rename docker images

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 cd keycloak-containers/server
 git checkout 13.0.0
-docker build . -t keycloak:13.0.0
+docker build . -t jboss/keycloak:13.0.0
 git checkout 9.0.3
-docker build . -t keycloak:9.0.3
+docker build . -t jboss/keycloak:9.0.3
 
 cd ../..
