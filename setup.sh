bf04d4c0c4ced9456183e93975276b9c531f42ac
Arno Schutijzer <arno.schutijzer@skryv.com>
2021-10-02 16:46:20 +0200

    only build 15.0.2

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 cd keycloak-containers/server
-git checkout 14.0.0
-docker build . -t jboss/keycloak:14.0.0
-git checkout 13.0.0
-docker build . -t jboss/keycloak:13.0.0
-git checkout 9.0.3
-docker build . -t jboss/keycloak:9.0.3
-git checkout 9.0.0
-docker build . -t jboss/keycloak:9.0.0
+git checkout 15.0.2
+docker build . -t jboss/keycloak:15.0.2
 
 cd ../..
