150635dab60c40232671af741db995d4fde06e72
Arno Schutijzer <arno.schutijzer@skryv.com>
2022-04-14 22:56:51 +0200

    upgrade to 16.1.1 & use official docker image name

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd keycloak-containers/server
-git checkout 15.0.2
-docker build . -t jboss/keycloak:15.0.2
+git checkout 16.1.1
+docker build . -t quay.io/keycloak/keycloak:16.1.1
 
 cd ../..
