ffda1946026736bcf9c692d65544cd467723fd7d
Arno Schutijzer <arno.schutijzer@skryv.com>
2022-04-14 23:05:19 +0200

    upgrade to 17.0.1

--- setup.sh
+++ setup.sh
@@ -4,4 +4,7 @@ cd keycloak-containers/server
 git checkout 16.1.1
 docker build . -t quay.io/keycloak/keycloak:16.1.1
 
+git checkout 4d7d01e0bb18e4f2b2593150955453d05bb0ede9
+docker build . -t quay.io/keycloak/keycloak:17.0.1
+
 cd ../..
